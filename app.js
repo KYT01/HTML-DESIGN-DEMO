483d6515b381a2aa216a67cc58df4344ee2dc93f
kumail imran <kumail2306@gmail.com>
2023-06-08 14:45:31 +0100

    javascript update

---
+++ app.js
@@ -0,0 +1,16 @@
+let userName = prompt("Hello User, please tell me your name");
+console.log(userName);
+
+const welcomeMsg = alert("Welcome to my page " + userName)
+
+let myNum = 10;
+
+if (myNum === 10) {
+    console.log("I love this!");
+} else if (myNum === 5) {
+    console.log("This work is good");
+} else {
+    console.log("I hate this");
+}
+
+    
