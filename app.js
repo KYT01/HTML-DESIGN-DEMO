87c57e64f9f447114c70131f31272272fb519684
kumail imran <kumail2306@gmail.com>
2023-06-09 15:51:00 +0100

    functions update

--- app.js
+++ app.js
@@ -3,14 +3,3 @@ console.log(userName);
 
 const welcomeMsg = alert("Welcome to my page " + userName)
 
-let myNum = 10;
-
-if (myNum === 10) {
-    console.log("I love this!");
-} else if (myNum === 5) {
-    console.log("This work is good");
-} else {
-    console.log("I hate this");
-}
-
-    
---
+++ function.js
@@ -0,0 +1,32 @@
+let user;
+
+function userName() {
+  if (!user) {
+    user = prompt("What is your name?");
+  }
+  let welcomeMessage = "Welcome " + user;
+  document.write(welcomeMessage);
+}
+
+userName();
+
+let rating;
+
+function displayRating() {
+  let rating = prompt("On a scale of 1 - 3, how much do you like the artwork?");
+  let message = "";
+
+  if (rating === "1") {
+    message = ":(";
+  } else if (rating === "2") {
+    message = ":)";
+  } else if (rating === "3") {
+    message = ":o";
+  }
+
+  document.write(message);
+}
+
+displayRating();
+
+
